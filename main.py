893ce24468b741e8750b0770013ef128d52e4856
OPI-py <oleksandrp09@gmail.com>
2021-02-20 22:49:57 +0200

    Add background color option for left bar

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ class Notepad:
     edit_menu = tk.Menu(menu_bar, tearoff=0)
     customize_menu = tk.Menu(menu_bar, tearoff=0)
     vertical_marker_menu = tk.Menu(customize_menu, tearoff=0)
+    left_bar_menu = tk.Menu(customize_menu, tearoff=0)
     theme_edit = tk.Menu(customize_menu, tearoff=0)
     help_menu = tk.Menu(menu_bar, tearoff=0)
     popup_menu = tk.Menu(root, tearoff=0)
@@ -75,6 +76,7 @@ class Notepad:
     
     variable_marker = tk.IntVar()
     variable_theme = tk.IntVar()
+    variable_left_bar = tk.IntVar()
     
     canvas_line = tk.Canvas(text_area, width=1, height=Height,
             highlightthickness=0, bg='lightsteelblue3')
@@ -82,7 +84,6 @@ class Notepad:
     statusbar = tk.Label(root, text=f"Line: 1 | Col: 0 | Symbols: 0",
         relief=tk.FLAT, anchor='e', highlightthickness=0)
     line_count_bar = LineNumbers(width=27, highlightthickness=0)
-    
     def __init__(self):
         self.root.title("Untitled - Notepad")
         # Center the window
@@ -142,6 +143,12 @@ class Notepad:
             menu=self.vertical_marker_menu)
         self.customize_menu.add_cascade(label='Color theme',
             menu=self.theme_edit)
+        self.customize_menu.add_cascade(label="Left bar color",
+            menu=self.left_bar_menu)
+        # Left bar color
+        self.left_bar_menu.add_checkbutton(label='LightSteelBlue', onvalue=1,
+            offvalue=0, variable=self.variable_left_bar,
+            command=self.left_bar_color)
         # Vertical marker
         self.vertical_marker_menu.add_checkbutton(label="80", onvalue=1,
             offvalue=0, variable=self.variable_marker,
@@ -360,6 +367,12 @@ class Notepad:
         self.statusbar.config(
             text=f"Line: {line} | Col: {col} | Symbols: {symb}")
         self.line_count_bar.redraw()
-        
+    
+    def left_bar_color(self, event=None):
+        if self.variable_left_bar.get() == 0:
+            self.line_count_bar.config(bg='SystemButtonFace')
+        elif self.variable_left_bar.get() == 1:
+            self.line_count_bar.config(bg='lightsteelblue3')
+
 notepad = Notepad()
 notepad.run()
