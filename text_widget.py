c740086badf26b5da3928f942ac9188fe0914f4b
OPI-py <oleksandrp09@gmail.com>
2021-05-19 17:39:44 +0300

    Update Exception

--- text_widget.py
+++ text_widget.py
@@ -12,17 +12,17 @@ class TextWidget(tk.Text):
     def __init__(self, *args, **kwargs):
         tk.Text.__init__(self, *args, **kwargs)
         # action for the underlying widget
-        self.first = self._w + "first"
-        self.tk.call("rename", self._w, self.first)
-        self.tk.createcommand(self._w, self.icursor_agent)
+        self._first = self._w + "_first"
+        self.tk.call("rename", self._w, self._first)
+        self.tk.createcommand(self._w, self._icursor_agent)
 
-    def icursor_agent(self, *args):
+    def _icursor_agent(self, *args):
         # perform requested action
-        command = (self.first,) + args
+        command = (self._first,) + args
         try:
             result = self.tk.call(command)
-        except Exception as e:
-            raise e
+        except Exception:
+            return None
 
         # generate an event when icursor move or edited or view is scrolled
         if (args[0] in ("insert", "replace", "delete") or 
@@ -33,4 +33,4 @@ class TextWidget(tk.Text):
             args[0:2] == ("yview", "scroll")):
             self.event_generate("<<IcursorModify>>", when="tail")
 
-        return result 
+        return result
